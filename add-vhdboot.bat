dbef630725b5aedd075d6d642c5ef573b3776710
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2021-12-15 02:40:09 +0900

    Update add-vhdboot.bat

--- add-vhdboot.bat
+++ add-vhdboot.bat
@@ -7,14 +7,15 @@ CD /D "%~dp0"
 SET vhdpath=
 SET /P vhdpath="vhd path?"
 
-for /F "delims=" %%i in (%filepath%) do set filename="%%~nxi"
+if not exist "%vhdpath%" ( echo %vhdpath% does not exists. & goto end )
+for /F "delims=" %%i in (%vhdpath%) do set filename="%%~nxi"
 
 SET name=
-SET /P name="entry name?"
+SET /P name="entry name?[%filename%]"
 
 if defined name goto exec
 
-
+set "name=%filename%"
 
 :exec
 powershell.exe -NoProfile -ExecutionPolicy Unrestricted .\add-vhdboot.ps1 "%name%" "%vhdpath%"
